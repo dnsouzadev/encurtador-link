5050c0d72afff9ebda0842e475b9fcd0590478a5
daniel souza <workdndsza@gmail.com>
2022-08-16 22:28:59 -0300

    terminando o shortener

--- app/Ulvis/urlShortener.php
+++ app/Ulvis/urlShortener.php
@@ -13,15 +13,35 @@ class UrlShortener {
 
     // executa a request na api do ulvis
     $response = self::send($params);
+
+    // retorna a url encurtada
+    return $response['data']['url'] ?? null
   }
 
     // método responsável por executar a request nas apido ulvis
   private static function send($params = []) {
     // endpoint completo
     $endpoint = 'https://ulvis.net/API/write/get?'.http_build_query($params);
-    echo "<pre>";
-    print_r($endpoint);
-    echo "</pre>"; exit;
+
+    // inicia o curl
+    $curl = curl_init();
+
+    // configura o curl
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $endpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+
+    // executa o curl
+    $response = curl_exec($curl);
+
+    //fecha a conexão com o curl
+    curl_close($curl);
+
+    // retorna o valor em array
+    return strlen($response) ? json_decode($response, true): [];
+
     }
 
 }
